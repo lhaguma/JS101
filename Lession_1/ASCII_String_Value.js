8225bbc1da6f85e23834271b78901f227b228cf1
Leocadie Haguma <lhaguma1@gmail.com>
2021-02-06 12:28:53 -0700

    Easy Problems

---
+++ Lession_1/ASCII_String_Value.js
@@ -0,0 +1,8 @@
+function asciiValue(str) {
+  return str.charCodeAt()
+}
+
+console.log(asciiValue('Four score'));         // 984
+console.log(asciiValue('Launch School'));      // 1251
+console.log(asciiValue('a'));                  // 97
+console.log(asciiValue(''));  
